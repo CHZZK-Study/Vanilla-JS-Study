505087709bf4c92e1331f975b02f45af1fc5e3cb
rkdcodus <rcd7275@naver.com>
2024-01-30 23:41:40 +0900

    refactor: textContent 대신 class 사용, color 배열 생성

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 const buttonContainter = document.getElementById("button-container");
-const htmlCount = document.getElementById("count");
+const countValue = document.getElementById("count");
+const color = ["green", "red", "black"];
 let count = 0;
 
 const update = () => {
-  htmlCount.innerText = `${count}`;
-  if (count > 0) htmlCount.style.color = "green";
-  if (count < 0) htmlCount.style.color = "red";
-  if (count === 0) htmlCount.style.color = "black";
+  countValue.innerText = `${count}`;
+  if (count > 0) countValue.style.color = color[0];
+  if (count < 0) countValue.style.color = color[1];
+  if (count === 0) countValue.style.color = color[2];
 };
 
 const counter = (e) => {
-  const click = e.target.textContent;
-  if (click === "DECREASE") count--;
-  if (click === "RESET") count = 0;
-  if (click === "INCREASE") count++;
+  const click = e.target.classList;
+  if (click.contains("decrease")) count--;
+  if (click.contains("reset")) count = 0;
+  if (click.contains("increase")) count++;
   update();
 };
 
