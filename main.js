a20f1f8a6867344787c31c153ce675518dd45345
rkdcodus <rcd7275@naver.com>
2024-02-07 01:19:14 +0900

    feat: 데이터 객체 배열 생성

---
+++ main.js
@@ -0,0 +1,30 @@
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://i.pinimg.com/236x/82/19/e9/8219e955fd50a0eb26959d17f4b173c7.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi eligendi odit ducimus, praesentium sed suscipit tempora, autem est quaerat nisi dolorem aperiam dolores nobis reiciendis? Animi quia veritatis aperiam perferendis.",
+  },
+  {
+    id: 2,
+    name: "anna smith",
+    job: "web designer",
+    img: "https://i.pinimg.com/564x/71/48/c9/7148c9737ab02a98e4c1ab22ead080a5.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi eligendi odit ducimus, praesentium sed suscipit tempora, autem est quaerat nisi dolorem aperiam dolores nobis reiciendis? ",
+  },
+  {
+    id: 3,
+    name: "Danish smith",
+    job: "frontend",
+    img: "https://i.pinimg.com/564x/63/31/18/633118d76f56a929855799e84bfd723e.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi eligendi odit ducimus, praesentium sed suscipit tempora, autem est quaerat nisi dolorem aperiam dolores nobis reiciendis? danish",
+  },
+  {
+    id: 4,
+    name: "any person",
+    job: "any job",
+    img: "https://i.pinimg.com/564x/d0/e6/00/d0e60042f48bee3d1839ce86acbf67be.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi eligendi odit ducimus ",
+  },
+];
