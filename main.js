5d547a7aef44151c06121483839416daa8fa0521
rkdcodus <rcd7275@naver.com>
2024-02-14 03:49:24 +0900

    feat: 창 너비에 따라 design 바꾸기

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menu = document.querySelector(".menu");
 const drop = document.querySelector(".drop");
-
+const wide = document.querySelector(".wide");
 const dropDown = () => {
   drop.classList.toggle("hide");
 };
 
+const resize = (e) => {
+  const width = e.currentTarget.innerWidth;
+  width < screen.width * 0.5 ? wide.classList.add("fold") : wide.classList.remove("fold");
+};
+
 menu.addEventListener("click", dropDown);
+window.addEventListener("resize", resize);
