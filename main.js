1f5bf26437f423af2e92b6395d7a287edc7f6d45
rkdcodus <rcd7275@naver.com>
2024-03-06 01:46:44 +0900

    ✨feat: 토글 기능에 따라 아이콘 변경하기

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const container = document.getElementById("container");
 const boxs = document.querySelectorAll(".box");
+const icons = document.querySelectorAll(".fa-regular");
 
 const toggle = (e) => {
+  console.log(e.target);
+  icons.forEach((icon) => {
+    if (e.target !== icon && icon.classList.contains("fa-square-minus")) {
+      icon.classList.remove("fa-square-minus");
+      icon.classList.add("fa-square-plus");
+    }
+  });
   const elem = e.target.closest(".box");
-
   elem.classList.toggle("open");
+
+  if (e.target.classList.contains("fa-square-plus")) {
+    e.target.classList.remove("fa-square-plus");
+    e.target.classList.add("fa-square-minus");
+  } else if (e.target.classList.contains("fa-square-minus")) {
+    e.target.classList.remove("fa-square-minus");
+    e.target.classList.add("fa-square-plus");
+  }
 
   boxs.forEach((box) => {
     if (elem !== box && box.classList.contains("open")) {
